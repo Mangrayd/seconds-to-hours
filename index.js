c0f9e8e90044dcf8b46f08c0ff838104b4ce82ef
Pilyaev Vadim <dr.pilyaev@yandex.ru>
2018-04-23 14:59:11 +0300

    - fixed index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function secToHours(sec){
+module.exports =  function secToHours(sec){
 	let h = parseInt( (sec/60)/60 );
 	let m_full = parseInt( sec/60 );
 	let m = m_full - h*60;
@@ -8,12 +8,11 @@ function secToHours(sec){
 		return String(val).length===1 ? ('0'+val) : val;
 	};
 
-	console.log( 'кол-во целых часов: ', h );
-	console.log( 'кол-во целых минут: ', m_full );
-	console.log( `кол-во минут: ${m_full}мин - ${h}ч (${h*60}мин) = ${m_full - h*60}` );
-	console.log( `кол-во секунд: ${sec}сек - ${m_full}мин (${m_full*60}сек) = ${sec - m_full*60}` );
-	console.log( `Result: ${h}:${seroPad(m)}:${seroPad(s)}` );
+	// console.log( 'кол-во целых часов: ', h );
+	// console.log( 'кол-во целых минут: ', m_full );
+	// console.log( `кол-во минут: ${m_full}мин - ${h}ч (${h*60}мин) = ${m_full - h*60}` );
+	// console.log( `кол-во секунд: ${sec}сек - ${m_full}мин (${m_full*60}сек) = ${sec - m_full*60}` );
+	// console.log( `Result: ${h}:${seroPad(m)}:${seroPad(s)}` );
 
-}
-
-module.exports = secToHours;
+	return `${h}:${seroPad(m)}:${seroPad(s)}`;
+};
