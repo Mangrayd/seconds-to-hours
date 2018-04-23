c52917b6502483db2d29325edcbcb586d24caf82
Pilyaev Vadim <dr.pilyaev@yandex.ru>
2018-04-23 15:23:44 +0300

    - fix index: wrong name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports =  function secToHours(sec){
 	let m = m_full - h*60;
 	let s = sec - m_full*60;
 
-	let seroPad = function(val){
+	let zeroPad = function(val){
 		return String(val).length===1 ? ('0'+val) : val;
 	};
 
@@ -14,5 +14,5 @@ module.exports =  function secToHours(sec){
 	// console.log( `кол-во секунд: ${sec}сек - ${m_full}мин (${m_full*60}сек) = ${sec - m_full*60}` );
 	// console.log( `Result: ${h}:${seroPad(m)}:${seroPad(s)}` );
 
-	return `${h}:${seroPad(m)}:${seroPad(s)}`;
+	return `${h}:${zeroPad(m)}:${zeroPad(s)}`;
 };
