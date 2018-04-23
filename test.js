144d3d5470ae1b2ddba8303e9d73140dae8fc273
Pilyaev Vadim <dr.pilyaev@yandex.ru>
2018-04-23 15:22:48 +0300

    - update mocha test

---
+++ test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const secToHours = require('./index.js');
+
+describe('Tests for "secToHours" function', () => {
+	it('testing seconds with zero', () => {
+		assert.equal('0:00:08', secToHours(8));
+	});
+	it('testing seconds without zero', () => {
+		assert.equal('0:00:59', secToHours(60-1));
+	});
+	it('testing minutes with zero', () => {
+		assert.equal('0:08:00', secToHours(60*8));
+	});
+	it('testing minutes without zero', () => {
+		assert.equal('0:59:00', secToHours(60*60-60));
+	});
+	it('testing hours (1)', () => {
+		assert.equal('1:02:11', secToHours(60*60+60*2+11));
+	});
+	it('testing hours (2)', () => {
+		assert.equal('61:02:11', secToHours(60*60*60+60*60+60*2+11));
+	});
+
+
+});
